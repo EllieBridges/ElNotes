8e1435c4bf82103cb1d8f8feec3701b8922eef06
Ellie Bridges <elliebridges@Ellies-MacBook-Pro.local>
2023-05-24 18:04:46 -0700

    Navbar added

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+export default function HomePage() {
+    return (
+        <div className=''>
+            <Navbar />
+            {/* <Title />
+            <NoteList /> */}
+        </div>
+    );
+};
+
