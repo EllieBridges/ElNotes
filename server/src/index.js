e95d06e37a4fb665abaa63c60026ae8ec2cdc4a4
Ellie Bridges <elliebridges@Ellies-MacBook-Pro.local>
2023-05-29 18:23:36 -0700

    created http handlers

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,38 @@
-const express = require('express')
+const { notes } = require('./notes')
+
+const express = require('express') //import
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello pig')
+
+// middleware - authentication/ logging - define before handlers
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next();
 })
 
-app.listen(3001)
+app.use(express.json())
 
+
+//route handlers
+app.post('/', (req, res) => {
+    notes.push(req.body)
+    res.send('Create endpoint')
+})
+
+app.get('/', (req, res) => {
+    res.send(notes)
+})
+
+// app.put('/', (req, res) => {
+//     res.send('Put endpoint')
+// })
+
+app.delete('/', (req, res) => {
+    notes.pop()
+    res.send('Delete endpoint')
+})
+
+app.listen(3001, () => { console.log('Server started') }) //serve
+
+
+// create new note POST, list existing notes GET and make changes to an existing note PUT, delete an existing note DELETE - if successful 200/201
---
+++ server/src/notes.js
@@ -0,0 +1,7 @@
+const notes = [{
+    id: 1,
+    title: 'My dream',
+    content: 'I had a very weird dream last night.'
+}]
+
+module.exports = { notes }
