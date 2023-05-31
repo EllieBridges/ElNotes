b23cdf8b71e89408587136db7fe254ef5a5d989c
Ellie Bridges <elliebridges@Ellies-MacBook-Pro.local>
2023-05-31 11:45:27 -0700

    created a map for notes added functionality to the http request methods - testing with Postman.

--- server/src/index.js
+++ server/src/index.js
@@ -1,38 +1,52 @@
-const { notes } = require('./notes')
+//import for the unique id - key for the 'notes' map
+const { v4: uuidv4 } = require('uuid');
 //import express web framework
 const express = require('express')
 const app = express()
 
+//used as an alternative to a database. 
+const notes = new Map()
+notes.set("993865e8-1797-4491-82d5-ce9286e98b76", {
+    title: 'My dream',
+    content: 'I had a very weird dream last night.'
+})
+
 //Always the same 'payload data type' - apply as a middleware - load BEFORE handlers
 app.use(express.json())
 
+
 // create handlers for each part of the app - http request methods - for creating, reading, editing and deleting 'register routes'
+const noteById = (id) => {
+    const note = notes.get(id)
+    return note;
+}
+
 
 app.post('/notes', (req, res) => {
-    // TODO: generate ID here, then save to notes array
-    notes.push(req.body)
-    res.send('Post - Note creation')
+    const newNote = req.body;
+    notes.set(uuidv4(), newNote)
+    res.send({ data: [...notes], message: 'Post - new note created' })
 });
 
 app.get('/notes', (req, res) => {
-    res.send({ data: notes, message: 'Get - notes retrieved' })
+    res.send({ data: [...notes], message: 'Get - notes retrieved' })
 });
 
 app.get('/notes/:noteId', (req, res) => {
-    const foundNote = notes.find(note => Number(req.params.noteId) === note.id)
-    console.log(foundNote)
+    const foundNote = noteById(req.params.noteId);
     res.send({ data: foundNote, message: 'Get - note retrieved' })
 });
 
 app.put('/notes/:noteId', (req, res) => {
-    // TODO: update an existing note by ID
-    res.send('Put - Note editor')
+    const newNote = req.body;
+    notes.set(req.params.noteId, { title: newNote.title, content: newNote.content })
+    res.send({ data: [...notes], message: 'Put - Note editor' })
 });
 
 app.delete('/notes/:noteId', (req, res) => {
-    // TODO: delete note by ID
-    notes.pop()
-    res.send('Delete - Note instance deleted')
+    const noteId = req.params.noteId
+    notes.delete(noteId)
+    res.send({ data: [...notes], message: 'Delete - Note instance deleted' })
 })
 
 //ensure ports for frontend and back end are different
--- server/src/notes.js
+++
@@ -1,7 +0,0 @@
-const notes = [{
-    id: 23,
-    title: 'My dream',
-    content: 'I had a very weird dream last night.'
-}]
-
-module.exports = { notes }
